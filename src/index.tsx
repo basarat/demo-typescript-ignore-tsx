d0b00af60c1909549cb2cc4f9a0f1fbea35c8718
basarat <basaratali@gmail.com>
2021-09-27 14:31:14 +1000

    wrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 
-// Intentional poor annotation
-const App = (props: { name: string }) => <div>{props.name || 'Default'}</div>;
+// Intentional poor annotation 
+//  e.g. in an external library
+const App = (props: { name: string }) =>
+  <div>{props.name || 'Default'}</div>;
 
-<App name="hello" />;
-
-// @ts-ignore
 <App />;
 
 <div>
-  {
-    // @ts-ignore
-    <App />
-  }
-</div>;
-
-// @ts-ignore
-const example = <App />;
-
-<div>
-  {example}
-</div>;
-
-
-<div>
-  <App
-    // @ts-ignore
-    name={123}
-  />
+  {/* @ts-ignore */}
+  <App />
 </div>;
